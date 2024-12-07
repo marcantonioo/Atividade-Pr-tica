b3eaa487c755aa1fadbe4c1bcc0e0f4e93b11eac
marco antonio <dinizsilvam3@gmail.com>
2024-12-06 23:15:59 -0300

    feat: add conditional evaluation

--- calculadora.js
+++ calculadora.js
@@ -1,22 +1,9 @@
-const soma = () => {
-    console.log(parseInt(args[1]) + parseInt(args[2]));
-};
+var x = args[0];
+var y = args[2];
+var operator = args[1];
 
-const sub = () => {
-   console.log(parseInt(args[1]) - parseInt(args[2]));
-};
-
-const args = process.argv.slice(2);
-
-switch (args[0]) {
-    case 'soma':
-        soma();
-        break;
-
-    case 'sub':
-        sub();
-        break;
-
-    default:
-        console.log('does not support', args[0]);
+function evaluate(param1, param2, operator) {
+  return eval(param1 + operator + param2);
 }
+
+if ( console.log( evaluate(x, y, operator) ) ) {}
